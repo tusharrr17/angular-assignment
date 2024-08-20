3e1beaf0fdece81b12b5da69658698c26532739d
Tushar Tembhurne <tushartembhurne17@gmail.com>
2024-08-20 21:33:45 +0530

    Add filter component

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
 
+  measures = [
+    { id: 'quantity', name: 'quantity' },
+    { id: 'sales', name: 'sales' }
+  ];
+  
+  attributes = [
+    { id: 'city', name: 'city' },
+    { id: 'country', name: 'country' },
+    { id: 'customer_id', name: 'customer_id' },
+    { id: 'item_type', name: 'item_type' },
+    { id: 'item_id', name: 'item_id' },
+    { id: 'product', name: 'product' },
+    { id: 'region', name: 'region' },
+    { id: 'SKU', name: 'SKU' },
+    { id: 'state', name: 'state' },
+    { id: 'store', name: 'store' },
+    { id: 'zip_code', name: 'zip_code' },
+    { id: 'dates', name: 'dates' }
+  ];
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  constructor(private router: Router) { }
+
+  navigateTo(route: string): void {
+    this.router.navigate([`/${route}`]);
+  }
+  
 }
