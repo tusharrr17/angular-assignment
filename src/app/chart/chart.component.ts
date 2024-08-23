a47a579d9289344636f95d7fb8befb445e37ffbe
Tushar Tembhurne <tushartembhurne17@gmail.com>
2024-08-23 05:37:06 +0530

    Add settings component

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-chart',
@@ -8,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrl: './chart.component.css'
 })
 export class ChartComponent {
+  public chart: any;
 
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart() {
+
+    this.chart = new Chart("MyChart", {
+      type: 'bar',  
+
+      data: {
+        labels: ['2022-05-10', '2022-05-11', '2022-05-12', '2022-05-13',
+          '2022-05-14', '2022-05-15', '2022-05-16', '2022-05-17',],
+        datasets: [
+          {
+            label: "Sales",
+            data: ['467', '576', '572', '79', '92',
+              '574', '573', '576'],
+            backgroundColor: 'blue'
+          },
+          {
+            label: "Profit",
+            data: ['542', '542', '536', '327', '17',
+              '0.00', '538', '541'],
+            backgroundColor: 'limegreen'
+          }
+        ]
+      },
+      options: {
+        aspectRatio: 2.5
+      }
+
+    });
+  }
 }
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  
+  selectedTab: string = 'chartType';
+  selectedChartOption: number | null = null;
+
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+  }
+
+  selectChartOption(option: number) {
+    this.selectedChartOption = option;
+  }
 
 }
